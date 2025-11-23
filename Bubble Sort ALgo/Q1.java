11cc8c0c101e03ad019bfb29b57ef64723a5a2f2
unknown <himanshuj053.com>
2025-11-23 19:14:17 +0530

    Modified

--- Bubble Sort ALgo/Q1.java
+++ Bubble Sort ALgo/Q1.java
@@ -21,8 +21,8 @@ public class Q1{
     static void BubbleSort(int[] arr){
         boolean Swapped = false;
         for(int i = 0;i<arr.length;i++){
-            Swapped = false;
-            for(int j = 1;j<arr.length - 1;j++){
+            // Swapped = false;
+            for(int j = 1;j<arr.length - i;j++){
                 if(arr[j]<arr[j-1]){
                     int temp = arr[j];
                     arr[j] = arr[j-1];
