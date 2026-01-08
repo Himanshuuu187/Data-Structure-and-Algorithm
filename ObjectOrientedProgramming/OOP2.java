08d5da8c1517e52997eb52661b965015c467db0d
unknown <himanshuj053.com>
2026-01-09 01:31:47 +0530

    ADDED

--- ObjectOrientedProgramming/OOP2.java
+++
@@ -1,9 +0,0 @@
-package ObjectOrientedProgramming;
-
-public class OOP2 {
-    public static void main(String[] args) {
-        Human h1 = new Human("Ravi",55 );
-        h1.disp();
-    }
-    
-}
